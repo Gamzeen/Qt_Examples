cc746917f89520019bed7390d0395be3917f1d82
Gamzen <74109466+Gamzeen@users.noreply.github.com>
2021-04-27 23:40:18 +0300

    Update basic_textbox_button.py

--- basic_textbox_button.py
+++ basic_textbox_button.py
@@ -1,3 +1,5 @@
+#you can check from here
+#https://birhankarahasan.com/pyqt-nedir-qt-designer-nedir-python-arayuz-olusturma
 import sys
 from PyQt5.QtWidgets import QWidget, QApplication, QLineEdit, QPushButton, QLabel
 
